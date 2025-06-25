ed256fa62b0e47abd7d63687a87420b1d6ac56d2
OsimAkash <osimakash123@gmail.com>
2025-06-25 23:25:02 +0600

    phython practice

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 # Phython Variable Decleare
 student_count = 1000
-rating = 4.99
+rating = 4.99s
 is_published = True
 course_name = "Phython Programming"
 print("students_count")
---
+++ dictionary.py
@@ -0,0 +1,4 @@
+
+data = {'Phython', 'Java', 'Kotlin','C','C++'}
+# data.remove('Kotlin')
+print(data)
